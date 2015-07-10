063f3ac45e519298a452bd42caf235353057870e
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-10 10:35:51 +0200

    deleted:    HTML5Application/nbproject/project.properties 	deleted:    HTML5Application/nbproject/project.xml 	deleted:    HTML5Application/public_html/index.html 	new file:   php/controlli/controllo_iscrizione.php

---
+++ php/controlli/controllo_iscrizione.php
@@ -0,0 +1,15 @@
+<?php
+        // put your code here
+	if(empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])){
+		
+		echo "Username, password e/o email mancante <br/>";
+	
+	}
+	else {
+		$usr = $_POST['username'];
+		$password = $_POST['password'];
+		$email = $_POST['email'];
+
+	}
+                    
+?>
