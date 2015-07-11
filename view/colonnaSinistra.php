7c3db3d4a275ad932d58c34595b1a7e45d5d4e5b
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-11 11:50:12 +0200

    modified:   view/colonnaSinistra.php

--- view/colonnaSinistra.php
+++ view/colonnaSinistra.php
@@ -1,5 +1,20 @@
 <div class='colonnaSinistra'>
 
+	<h3 class="categorieTitolo">
+		Categorie
+	</h2>	
+	
+	<p class="categorieElenco">
+	
+		<br>
+			<ul>
+				<li><a href="" alt="Libri">Libri</a></li>
+				<li><a href="" alt="Elettronica">Elettronica</a></li>
+				<li><a href="" alt="Audio e video">Audio e video</a></li>
+				<li><a href="" alt="Tempo libero">Tempo libero</a></li>
+			</ul>
+		</br>
+	</p>
 		
 	
 </div>
