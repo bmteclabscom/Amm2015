816086dbbc918a280de93ec2a580298cbbb00ffd
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-11 11:55:35 +0200

    modified:   view/colonnaSinistra.php 	modified:   view/css/style.css

--- view/colonnaSinistra.php
+++ view/colonnaSinistra.php
@@ -4,16 +4,13 @@
 		Categorie
 	</h2>	
 	
-	<p class="categorieElenco">
-	
-		<br>
-			<ul>
-				<li><a href="" alt="Libri">Libri</a></li>
-				<li><a href="" alt="Elettronica">Elettronica</a></li>
-				<li><a href="" alt="Audio e video">Audio e video</a></li>
-				<li><a href="" alt="Tempo libero">Tempo libero</a></li>
-			</ul>
-		</br>
+	<p class="categorieElenco">		
+		<ul>
+			<li><a href="" alt="Libri">Libri</a></li>
+			<li><a href="" alt="Elettronica">Elettronica</a></li>
+			<li><a href="" alt="Audio e video">Audio e video</a></li>
+			<li><a href="" alt="Tempo libero">Tempo libero</a></li>
+		</ul>
 	</p>
 		
 	
