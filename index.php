5cdafcf45b3d27c3899d2e69ca71d47a8ba50abb
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-11 11:03:27 +0200

    modified:   index.php 	modified:   view/css/style.css 	modified:   view/head.php 	new file:   view/help.php 	modified:   view/testata.php

--- index.php
+++ index.php
@@ -10,15 +10,7 @@
 
 	<head>
 		
-		<meta http-equip="Content-Type" content="text/html; charset=utf-8">
-		
-		<!-- selezione degli stili css-->
-		<link rel="stylesheet" type="text/css"	href="view/css/style.css" media="screen">	
-		<link rel="stylesheet" type="text/css"	href="view/css/background.css" media="screen">	
-		<link rel="stylesheet" type="text/css"	href="view/css/testo.css" media="screen">	
-
-
-		<title>GoodStore.net</title>  <!-- il titolo è un richiamo non troppo velato. AHAH-->
+		<?php include('view/head.php'); ?>
 		
 	</head>
 
