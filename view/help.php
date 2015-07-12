40968ddd426c3f18e71b4710005a4d8a55ad4328
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-12 19:16:43 +0200

    modified:   view/help.php

--- view/help.php
+++ view/help.php
@@ -1,18 +1,12 @@
 <div class="help"> 
 	
-	<h2>	Progetto di Amministrazione di Sistema a.a. 2014/2015 </h1>
+	<h2>Progetto di Amministrazione di Sistema a.a. 2014/2015 </h1>
 	
-	<h3>	Descrizione progetto</h2>
-	<p>	Il progetto è relativo a un piccolo webstore dove, in base ai privilegi utente, 
+	<h3>Descrizione progetto</h2>
+	<p>Il progetto è relativo a un piccolo webstore dove, in base ai privilegi utente, 
 	è possibile acquistare o mettere in vendita degli oggetti, caricando una descrizione, il prezzo e relativa foto.</p> 
 
-	<h3>	Elenco requisiti soddisfatti</h2>
-	<p>
-		<ul>
-			<li>Utilizzo di HTML e CSS</li>
-			<li>Utilizzo di PHP e MySQL</li>
-		</ul>
-	
-	</p>
+	<h3>Elenco requisiti soddisfatti</h2>
+	<p>Utilizzo di HTML e CSS <br>Utilizzo di PHP e MySQL</p>
 
 </div>
