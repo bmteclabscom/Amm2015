ca8aee8931b7b616db31a390d41362e5002a33b2
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-12 18:43:02 +0200

    modified:   view/css/testo.css 	modified:   view/help.php

---
+++ view/help.php
@@ -0,0 +1,18 @@
+<div class="help"> 
+	
+	<h1>Progetto Amministrazione di Sistema a.a. 2014/2015 </h1>
+	
+	<h2>Descrizione progetto</h2>
+	<p>Il progetto è relativo a un piccolo webstore dove, in base ai privilegi utente, 
+	è possibile acquistare o mettere in vendita degli oggetti, caricando una descrizione, il prezzo e relativa foto.</p> 
+
+	<h2>Elenco requisiti soddisfatti</h2>
+	<p>
+		<ul>
+			<li>Utilizzo di HTML e CSS</li>
+			<li>Utilizzo di PHP e MySQL</li>
+		</ul>
+	
+	</p>
+
+</div>
