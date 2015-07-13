f041ce4326252dcc2fd430f8c69b5364de416957
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-13 17:27:39 +0200

    modified:   php/mostraElettronica.php 	modified:   view/chisiamo.php

--- php/mostraElettronica.php
+++ php/mostraElettronica.php
@@ -3,7 +3,10 @@
 	$connection = mysql_connect('localhost', 'root', 'davide'); //apre la connessione col database
 	mysql_select_db('database1');	
 
-	$query = "SELECT * FROM articoli"; 	//seleziona tutti gli oggetti
+	$query = "SELECT articoli.nome, articoli.descrizione, articoli.prezzo, elettronica.marca,
+					elettronica.memoria1 FROM articoli JOIN elettronica 
+					ON articoli.nome = elettronica.nome"; 	//seleziona tutti gli oggetti con questa query
+					
 	$result = mysql_query($query);		//risultati della ricerca
 
 	echo "<table>"; 	//inizia la tabella
