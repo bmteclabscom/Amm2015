302285975f1d161fe0a1e61c44f124a9c619284b
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-13 13:54:43 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 				
 				<?php include('view/colonnaSinistra.php'); ?>
 				<?php include('view/colonnaDestra.php'); ?>
-				<?php include('view/help.php'); ?>
+				<?php include('view/login.php'); ?>
 
 				</div>
 		</div>
