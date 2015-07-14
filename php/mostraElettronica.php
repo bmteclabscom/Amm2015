66b69b011a11ddabd16a17bd7a015633a8f82de3
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-14 11:12:08 +0200

    modified:   php/mostraElettronica.php 	modified:   view/help.php

--- php/mostraElettronica.php
+++ php/mostraElettronica.php
@@ -5,7 +5,7 @@
 
 	$query = "SELECT articoli.nome, articoli.descrizione, articoli.prezzo, elettronica.marca,
 					elettronica.memoria1 FROM articoli JOIN elettronica 
-					ON articoli.nome = elettronica.nome"; 	//seleziona tutti gli oggetti con questa query
+					ON articoli.id = elettronica.id"; 	//seleziona tutti gli oggetti con questa query
 					
 	$result = mysql_query($query);		//risultati della ricerca
 
@@ -13,8 +13,8 @@
 
 	while($row = mysql_fetch_array($result)){   	//forma la tabella finché ci sono oggetti nel database
 		
-		echo "<tr><td>" . $row['nome'] . "</td><td>" . $row['descrizione'] .
-			"</td><td>" . $row['prezzo'] . "</td><td>" . $row['categoria'] ."</td></tr>";  
+		echo "<tr><td>" . $row['articoli.nome'] . "</td><td>" . $row['articoli.articoli.descrizione'] .
+			"</td><td>" . $row['prezzo'] . "</td><td>" . $row['elettronica.marcasc'] ."</td></tr>";  
 	
 	}
 
--- view/help.php
+++ view/help.php
@@ -3,8 +3,8 @@
 	<h2>Progetto di Amministrazione di Sistema a.a. 2014/2015 </h1>
 	
 	<h3>Descrizione progetto</h2>
-	<p>Il progetto è relativo a un piccolo webstore dove, in base ai privilegi utente, 
-	è possibile acquistare o mettere in vendita degli oggetti, caricando una descrizione, il prezzo e relativa foto.</p> 
+	<p>Il progetto &egrave; relativo a un piccolo webstore dove, in base ai privilegi utente, 
+	&egrave; possibile acquistare o mettere in vendita degli oggetti, caricando una descrizione, il prezzo e relativa foto.</p> 
 
 	<h3>Elenco requisiti soddisfatti</h2>
 	<p>Utilizzo di HTML e CSS <br>Utilizzo di PHP e MySQL</p>
