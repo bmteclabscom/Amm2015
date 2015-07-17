ecf8b931589c1ca69549d6b2a5143618af0bd4c7
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-17 19:23:06 +0200

    modified:   php/login_validation.php

--- php/login_validation.php
+++ php/login_validation.php
@@ -2,6 +2,15 @@
 
 	include("ConnessioneDatabase");		//connessione al database 
 	
+	$preparedStatement = $mysqli->stmt_init();	//prepared statement per evitare accessi non autorizzati tramite SQLinjection
+	
+	$loginQuery = "SELECT username, password FROM login WHERE user =? AND password =? ";	//query di verifica login
+	
+	$preparedStatement->prepare($loginQuery);	// preparo lo statement per l'esecuzione
+	
+	$preparedStatement->bind_param("ss", $_POST["username"], $_POST["password"]); // collego i parametri della query con il loro tipo
+	
+	
 	if(empty($_POST["username"]) || empty($_POST["password"]) ){	//se i campi sono vuoti
 		
 		echo "Username o password mancanti <br/>";
@@ -11,7 +20,8 @@
 		$username = $_POST["username"];
 		$password = $_POST["password"];
 		
-		$accessoSito = mysql_query("SELECT username, password FROM login WHERE user = '$username' AND password = '$password'");
+		$accessoSito = mysql_query($preparedStatement->execute());	//esegui la query
+
 		
 		if ($result > 0){	//se la risposta è affermativa
 			
