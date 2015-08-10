4a68f440ddb82b35f53b6cfe138d641338525bfe
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-08-10 17:12:00 +0200

    new file:   css/style.css 	modified:   game.php 	new file:   images/unicacasino700.png

---
+++ game.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+
+<html>
+
+	<head>
+		
+		<meta http-equip="Content-Type" content="text/html; charset=utf-8">
+		
+		<!-- selezione degli stili css-->
+		<link rel="stylesheet" type="text/css"	href="css/style.css" media="screen">	
+
+
+		<title>UniCA Casinò</title>  
+		
+	</head>
+
+	<body>
+
+		<div class='homepage'>
+			
+
+		
+			<div class='colonna'>
+				
+
+				</div>	<!--chiusura class colonne -->
+
+		</div>	<!--chiusura class homepage-->
+		
+		<!-- visualizzazione del footer -->
+
+
+	</body> 
+	 
+ </html>
