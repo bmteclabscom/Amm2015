29acf6d470cc359156295114dc7bc3a77ed350f4
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-08-10 21:05:34 +0200

    modified:   login.php

--- login.php
+++ login.php
@@ -24,9 +24,25 @@
 		
 			<div class='login'>
 				
-				
+				<form class="form" action="" method="POST">
+		
+					<p class='loginForm'>
+						
+						<label>Username: 
+							<input type='text' name='username' placeholder='Username' maxlength='15' title="Inserisci l' username" required>
+						</label> <br>
+						
+						<label>Password: 
+							<input class='passwordForma' type='password' placeholder='Password' name='password' maxlength='20' title='Inserisci la password' required>
+						</label><br>
+						
+						<input class='loginBotton' type='submit' name='login' value='Accedi'>
+					</p>
+					
+					
+				</form>
 
-				</div>	<!--chiusura class colonne -->
+			</div>	<!--chiusura class colonne -->
 
 		</div>	<!--chiusura class homepage-->
 		
